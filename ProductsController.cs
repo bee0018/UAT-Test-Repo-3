3ec397c15a260e3355bbfda865cb05be5995c910
Bryce Ellis <bryceellis24@gmail.com>
2026-09-20 17:12:24 -0500

    This is a commit to start a merge conflict for UAT testing

--- ProductsController.cs
+++ ProductsController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsWebAPI.Responses;
 
-namespace SportsWebAPI.Controllers
+namespace SportsWebAPI.Controllers // This is a comment to start a merge conflict
 {
     [ApiController]
     [Route("api/[controller]")]
