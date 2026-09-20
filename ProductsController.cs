6e2c12bb6cec8c98269175c03db3c1a0fd9a3c1c
Bryce Ellis <bryceellis24@gmail.com>
2026-09-20 16:45:34 -0500

    Added sample product controller to test repo

---
+++ ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsWebAPI.Responses;
+
+namespace SportsWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(TodoResponse), StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<string>> GetProducts()
+        {
+            TodoResponse response = new()
+            {
+                IsDone = true,
+                Todo = "This is a todo"
+            };
+
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> GetProductsById(int id)
+        {
+            return $"Product {id}";
+        }
+
+        [HttpPost]
+        public ActionResult<string> AddProduct([FromBody] string value)
+        {
+            return $"Posted: {value}";
+        }
+    }
+}
